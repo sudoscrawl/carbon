b483622fc9a690ee8302a0b393986a2c8e831bab
Ayush Kumar <ayush438500@gmail.com>
2026-07-06 20:28:29 +0530

    feat:Add parameter descriptions for moderation commands

--- app/modules/commands/moderation.py
+++ app/modules/commands/moderation.py
@@ -12,6 +12,8 @@ class Moderation(commands.Cog):
         self.bot = bot
         self.service = ModCmdService(self.bot)
 
+    # KICK COMMAND
+
     @app_commands.command(
         name="kick",
         description=app_commands.locale_str(_("Kick someone from this server.")),
@@ -19,6 +21,10 @@ class Moderation(commands.Cog):
     @app_commands.guild_only()
     @app_commands.checks.bot_has_permissions(kick_members=True)
     @app_commands.checks.has_permissions(kick_members=True)
+    @app_commands.describe(
+        target=app_commands.locale_str(_("The user you want to kick.")),
+        reason=app_commands.locale_str(_("Your reason to kick them. (Optional)")),
+    )
     async def kick(
         self,
         interaction: discord.Interaction,
@@ -27,6 +33,8 @@ class Moderation(commands.Cog):
     ):
         await self.service._kick(interaction, target, reason)
 
+    # BAN COMMAND
+
     @app_commands.command(
         name="ban",
         description=app_commands.locale_str(_("Ban someone from this server.")),
@@ -34,6 +42,10 @@ class Moderation(commands.Cog):
     @app_commands.guild_only()
     @app_commands.checks.bot_has_permissions(ban_members=True)
     @app_commands.checks.has_permissions(ban_members=True)
+    @app_commands.describe(
+        target=app_commands.locale_str(_("The user you want to ban.")),
+        reason=app_commands.locale_str(_("Your reason to ban them. (Optional)")),
+    )
     async def ban(
         self,
         interaction: discord.Interaction,
@@ -42,6 +54,8 @@ class Moderation(commands.Cog):
     ):
         await self.service._ban(interaction, target, reason)
 
+    # UNBAN COMMAND
+
     @app_commands.command(
         name="unban",
         description=app_commands.locale_str(_("Unban someone from this server.")),
@@ -49,6 +63,12 @@ class Moderation(commands.Cog):
     @app_commands.guild_only()
     @app_commands.checks.bot_has_permissions(ban_members=True)
     @app_commands.checks.has_permissions(ban_members=True)
+    @app_commands.describe(
+        user_id=app_commands.locale_str(
+            _("Discord ID of the person you want to unban.")
+        ),
+        reason=app_commands.locale_str(_("Your reason to unban them. (Optional)")),
+    )
     async def unban(
         self,
         interaction: discord.Interaction,
@@ -57,6 +77,8 @@ class Moderation(commands.Cog):
     ):
         await self.service._unban(interaction, user_id, reason)
 
+    # TIMEOUT COMMAND
+
     @app_commands.command(
         name="timeout",
         description=app_commands.locale_str(_("Timeout someone in this server.")),
@@ -64,6 +86,13 @@ class Moderation(commands.Cog):
     @app_commands.guild_only()
     @app_commands.checks.bot_has_permissions(moderate_members=True)
     @app_commands.checks.has_permissions(moderate_members=True)
+    @app_commands.describe(
+        target=app_commands.locale_str(_("The user you want to timeout.")),
+        duration=app_commands.locale_str(
+            _("The duration for which you want to timeout them.")
+        ),
+        reason=app_commands.locale_str(_("Your reason to timeout them. (Optional)")),
+    )
     async def timeout(
         self,
         interaction: discord.Interaction,
@@ -73,12 +102,18 @@ class Moderation(commands.Cog):
     ):
         await self.service._timeout(interaction, target, duration, reason)
 
+    # WARN COMMAND
+
     @app_commands.command(
         name="warn",
         description=app_commands.locale_str(_("Warn a member of this server.")),
     )
     @app_commands.guild_only()
     @app_commands.checks.has_permissions(manage_messages=True)
+    @app_commands.describe(
+        target=app_commands.locale_str(_("The user you want to warn.")),
+        reason=app_commands.locale_str(_("Your reason to warn them. (Optional)")),
+    )
     async def warn(
         self,
         interaction: discord.Interaction,
